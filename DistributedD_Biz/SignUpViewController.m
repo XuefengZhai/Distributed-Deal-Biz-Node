2a40fe21fd88c4f972f15ec2eff8d83ed9a9a04e
XuefengZhai <xzhai@cmu.edu>
2014-04-24 20:03:31 -0400

    Change UI

--- DistributedD_Biz/SignUpViewController.m
+++ DistributedD_Biz/SignUpViewController.m
@@ -8,9 +8,20 @@
 
 #import "SignUpViewController.h"
 
+#include <ifaddrs.h>
+#include <arpa/inet.h>
+#include <net/if.h>
+
+#define IOS_CELLULAR    @"pdp_ip0"
+#define IOS_WIFI        @"en0"
+#define IP_ADDR_IPv4    @"ipv4"
+#define IP_ADDR_IPv6    @"ipv6"
+
+
 @interface SignUpViewController ()
 {
     NSString *lat,*lng;
+    NSString *IPAdd;
     
 }
 
@@ -86,9 +97,10 @@
     NSString *pswText = psw.text;
     NSString *nameText = name.text;
     NSString *ageText = age.text;
+    IPAdd = [self getIPAddress:true];
     NSString *urlFinal = [NSString stringWithFormat:
-                          @"%@Email=%@&Psw=%@&Name=%@&Disc=%@&Location=%@+%@",
-                          urlBaseString,emailText,pswText,nameText,ageText,lat,lng];
+                          @"%@Email=%@&Psw=%@&Name=%@&Disc=%@&Location=%@+%@&IP=%@",
+                          urlBaseString,emailText,pswText,nameText,ageText,lat,lng,IPAdd];
     
     NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
     [request setURL:[NSURL URLWithString:urlFinal]];
@@ -150,6 +162,54 @@
                           otherButtonTitles:nil];
     [alert show];
 }
+
+- (NSString *)getIPAddress:(BOOL)preferIPv4
+{
+    NSArray *searchArray = preferIPv4 ?
+    @[ IOS_WIFI @"/" IP_ADDR_IPv4, IOS_WIFI @"/" IP_ADDR_IPv6, IOS_CELLULAR @"/" IP_ADDR_IPv4, IOS_CELLULAR @"/" IP_ADDR_IPv6 ] :
+    @[ IOS_WIFI @"/" IP_ADDR_IPv6, IOS_WIFI @"/" IP_ADDR_IPv4, IOS_CELLULAR @"/" IP_ADDR_IPv6, IOS_CELLULAR @"/" IP_ADDR_IPv4 ] ;
+    
+    NSDictionary *addresses = [self getIPAddresses];
+    //NSLog(@"addresses: %@", addresses);
+    
+    __block NSString *address;
+    [searchArray enumerateObjectsUsingBlock:^(NSString *key, NSUInteger idx, BOOL *stop)
+     {
+         address = addresses[key];
+         if(address) *stop = YES;
+     } ];
+    return address ? address : @"0.0.0.0";
+}
+
+- (NSDictionary *)getIPAddresses
+{
+    NSMutableDictionary *addresses = [NSMutableDictionary dictionaryWithCapacity:8];
+    
+    // retrieve the current interfaces - returns 0 on success
+    struct ifaddrs *interfaces;
+    if(!getifaddrs(&interfaces)) {
+        // Loop through linked list of interfaces
+        struct ifaddrs *interface;
+        for(interface=interfaces; interface; interface=interface->ifa_next) {
+            if(!(interface->ifa_flags & IFF_UP) || (interface->ifa_flags & IFF_LOOPBACK)) {
+                continue; // deeply nested code harder to read
+            }
+            const struct sockaddr_in *addr = (const struct sockaddr_in*)interface->ifa_addr;
+            if(addr && (addr->sin_family==AF_INET || addr->sin_family==AF_INET6)) {
+                NSString *name = [NSString stringWithUTF8String:interface->ifa_name];
+                char addrBuf[INET6_ADDRSTRLEN];
+                if(inet_ntop(addr->sin_family, &addr->sin_addr, addrBuf, sizeof(addrBuf))) {
+                    NSString *key = [NSString stringWithFormat:@"%@/%@", name, addr->sin_family == AF_INET ? IP_ADDR_IPv4 : IP_ADDR_IPv6];
+                    addresses[key] = [NSString stringWithUTF8String:addrBuf];
+                }
+            }
+        }
+        // Free memory
+        freeifaddrs(interfaces);
+    }
+    return [addresses count] ? addresses : nil;
+}
+
 
 
 
